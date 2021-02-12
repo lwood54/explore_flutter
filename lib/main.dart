0197a35a9cac4206ae706cdc7c207ff9835bf5fa
Logan Wood <lwood3499@gmail.com>
2021-02-11 22:22:15 -0600

    exploring basic functions, buttons, styling

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 // 2. Add a button which changes the text (to any other text)
 // 3. Split the app into 3 widgets, App, TextControl, and Text
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+// import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,51 +10,110 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
+      title: 'Changing the Text',
+      home: ChangeText(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class ChangeText extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _ChangeTextState createState() => _ChangeTextState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
+class _ChangeTextState extends State<ChangeText> {
+  String _textToChange = 'Starting text.';
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
+  void handleChangeText() {
+    if (_textToChange == 'Starting text.') {
+      setState(() {
+        _textToChange = 'This has changed';
+      });
+    } else {
+      setState(() {
+        _textToChange = 'Starting text.';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
+        appBar: AppBar(
+          title: Text('Change the Text Button'),
+        ),
+        body: Container(
+            height: 75.0,
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            decoration: BoxDecoration(color: Colors.green[300]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        _textToChange,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Expanded(
+                        flex: 1,
+                        child: ElevatedButton(
+                          child: Text(
+                            'change it!',
+                            textAlign: TextAlign.center,
+                          ),
+                          onPressed: handleChangeText,
+                        ))
+                  ],
+                ),
+              ],
+            )));
   }
 }
+
+// class RandomWords extends StatefulWidget {
+//   @override
+//   _RandomWordsState createState() => _RandomWordsState();
+// }
+
+// class _RandomWordsState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+//   final _biggerFont = TextStyle(fontSize: 18.0);
+
+//   Widget _buildSuggestions() {
+//     return ListView.builder(
+//         padding: EdgeInsets.all(16.0),
+//         itemBuilder: /*1*/ (context, i) {
+//           if (i.isOdd) return Divider(); /*2*/
+
+//           final index = i ~/ 2; /*3*/
+//           if (index >= _suggestions.length) {
+//             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+//           }
+//           return _buildRow(_suggestions[index]);
+//         });
+//   }
+
+//   Widget _buildRow(WordPair pair) {
+//     return ListTile(
+//       title: Text(
+//         pair.asPascalCase,
+//         style: _biggerFont,
+//       ),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Startup Name Generator'),
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   }
+// }
