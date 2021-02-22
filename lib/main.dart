389e496e5239820cb17fa225e7d789d24c4dcd14
Logan Wood <lwood3499@gmail.com>
2021-02-21 23:28:21 -0600

    91-99: add theme styles and map amount tracking

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,44 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+Map<int, Color> color = {
+  50: Color.fromRGBO(80, 179, 72, .1),
+  100: Color.fromRGBO(80, 179, 72, .2),
+  200: Color.fromRGBO(80, 179, 72, .3),
+  300: Color.fromRGBO(80, 179, 72, .4),
+  400: Color.fromRGBO(80, 179, 72, .5),
+  500: Color.fromRGBO(80, 179, 72, .6),
+  600: Color.fromRGBO(80, 179, 72, .7),
+  700: Color.fromRGBO(80, 179, 72, .8),
+  800: Color.fromRGBO(80, 179, 72, .9),
+  900: Color.fromRGBO(80, 179, 72, 1),
+};
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        accentColor: Colors.cyanAccent,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            )),
+        appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                    headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ))),
+        // primarySwatch: MaterialColor(0xFF50B348, color),
+        // accentColor: MaterialColor(0xFF7D85CA, color),
+      ),
       home: HomePage(),
     );
   }
@@ -22,18 +55,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 52.25,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Coffee',
-      amount: 7.99,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: 't1',
+    //   title: 'New Shoes',
+    //   amount: 52.25,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Coffee',
+    //   amount: 7.99,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   void _handleAddTransaction(String title, double amount) {
@@ -64,7 +97,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,51 +10,68 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
-          itemBuilder: (ctx, i) {
-            return Card(
-              child: Row(
+        child: transactions.isEmpty
+            ? Column(
                 children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    )),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${transactions[i].amount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple),
-                    ),
+                  Text(
+                    'No Transactions added yet!',
+                    style: Theme.of(context).textTheme.headline6,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        '${transactions[i].title}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMd().format(transactions[i].date),
-                        style: TextStyle(
-                          fontSize: 10,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  )
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 200,
+                    child: Image.asset('assets/images/waiting.png',
+                        fit: BoxFit.cover),
+                  ),
                 ],
-              ),
-            );
-          },
-          itemCount: transactions.length,
-        ));
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, i) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            // color: Colors.purple,
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          )),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            '\$${transactions[i].amount.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              '${transactions[i].title}',
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                            Text(
+                              DateFormat.yMd().format(transactions[i].date),
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+                itemCount: transactions.length,
+              ));
   }
 }
